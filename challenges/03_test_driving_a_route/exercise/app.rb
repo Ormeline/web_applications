f95e427f32096b181b526e658043f632618a65fd
Ormeline <ormelineluyengi@outlook.com>
2023-05-03 21:35:41 +0100

    answer to test_drive exercise- test passed

--- challenges/03_test_driving_a_route/exercise/app.rb
+++ challenges/03_test_driving_a_route/exercise/app.rb
@@ -2,5 +2,16 @@
 require "sinatra/base"
 
 class Application < Sinatra::Base
-  
+  get "/names" do
+    name = params[:name]
+    p name
+    "Julia, Mary, Karim"
+  end
+
+  # Catch-all route handler for 404 errors.
+  # This must be the last route defined in the class.
+  not_found do
+    status 404
+    "Page not found"
+  end
 end
