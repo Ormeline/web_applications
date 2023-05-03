8c1f643eab597dbe5cb61dfb84fbed8118536e2b
Ormeline <ormelineluyengi@outlook.com>
2023-05-03 20:05:36 +0100

    challenge task

--- challenges/01_building_a_route/hello_web_project_challenge/app.rb
+++ challenges/01_building_a_route/hello_web_project_challenge/app.rb
@@ -1,23 +1,12 @@
 # file: app.rb
-require 'sinatra/base'
-require 'sinatra/reloader'
+require "sinatra/base"
 
 class Application < Sinatra::Base
-  # This allows the app code to refresh
-  # without having to restart the server.
-  configure :development do
-    register Sinatra::Reloader
-  end
-
-  # Declares a route that responds to a request with:
-  #  - a GET method
-  #  - the path /
-  get '/' do
-    # The code here is executed when a request is received and we need to 
-    # send a response. 
-
-    # We can return a string which will be used as the response content.
-    # Unless specified, the response status code will be 200 (OK).
-    return 'Some response data'
+  post "/submit" do
+    name = params[:name]
+    message = params[:message]
+    p name
+    p message
+    "Thanks #{params[:name]}, you sent this message: \'#{params[:message]}\'"
   end
 end
