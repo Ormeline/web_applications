fad298e534744bd7001457991a87ea4940fbb5f7
Ormeline <ormelineluyengi@outlook.com>
2023-05-03 11:58:49 +0100

    building_a_route exercise

--- app.rb
+++ app.rb
@@ -1,23 +1,46 @@
 #this is the main file of our application. It will contain the applicaiton  class 
 
 require 'sinatra/base'
-require 'sinatra/reloader'
+#require 'sinatra/reloader'
 
 class Application < Sinatra::Base 
+
     # < this symble means the Application class is made to be a subclass of that class that is provided by the Sinatra. THis means our application class will inerate from all the methods and features from the sinatra bass class 
-    configure :development do
-        register Sinatra::Reloader
-    end
+    #configure :development do
+    #    register Sinatra::Reloader
+    #end
 
     # Declares a route that responds to a request with:
     #  - a GET method
     #  - the path /
     get '/' do
-    # The code here is executed when a request is received and we need to 
-    # send a response. 
+        return 'Hello!'
+    end 
 
-    # We can return a string which will be used as the response content.
-    # Unless specified, the response status code will be 200 (OK).
-    return 'Some response data'
-  end
+    get '/posts' do
+        greeting = params[:greeting]
+        name = params[:name]
+        
+        p greeting 
+        p name
+
+        return 'Hello Leo'
+    end
+
+    get '/posts' do
+        name = params[:name]
+        cohort_name = params[:cohort_name]
+
+        p name
+        p cohort_name
+
+        return 'A list of posts'
+    end
+
+    post '/posts' do
+        return 'Post was created'
+    end
 end
+
+
+
